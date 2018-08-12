1b0ae28b6d723d9ddbd324929616cdb18f3436d0
Josue Valencia <josue.valencia@domo.ec>
2018-08-12 19:28:04 +0200

    db json updated

---
+++ src/actions/types.js
@@ -0,0 +1,9 @@
+export function changeColor (color){
+// This will help our reducers.
+   return {
+    type:"CHANGE_COLOR",
+    color:color
+  }
+};
+
+export const FETCH_PHONES = 'FETCH_PHONES'; //

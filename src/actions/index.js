ae3440c1afc41d0afa8f001d0d6e26fae27c5a1e
Josue Valencia <josue.valencia@domo.ec>
2018-08-12 19:39:47 +0200

    Fetching phones

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
+import {FETCH_PHONES} from './types';
+
+export function fetchPhones (){
+console.log('fetching phones');
+  return(dispatch) => {
+    return axios.get("https://my-json-server.typicode.com/josuevalrob/phonecat/phones")
+      .then((phones)=> dispatch ({
+        type: FETCH_PHONES,
+        payload: phones
+      }))
+  }
+}
 
 export function loadColor (){
-// dispatch is returning something
+console.log('fetching');
   return(dispatch) => {
     return axios.get("http://www.colr.org/json/color/random")
       .then((response)=>{

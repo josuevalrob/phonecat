f4dd97c50ae6eaabfe6817e0b83852edf03450db
Josue Valencia <josue.valencia@domo.ec>
2018-08-12 19:57:12 +0200

    reducer working. Fetching phones!

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,29 @@
-let defaultState = {
-  color:"red"
-}
+// define un estado
+import { FETCH_PHONES } from '../actions/types';
 
+const defaultState = {
+  color:"red",
+  // traemos los diferentes telefonos dentro de un arreglo
+  items: []
+}
+// dependiendo del tipo de acción, cambia el estado.
 const mainReducer=(state=defaultState,action)=>{
-  if(action.type==="CHANGE_COLOR"){
-    return{
-      ...state,
-      color: action.color
-    }
-  }else {
-    return{
-      ...state
-    }
+
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      console.log('Changing color');
+      return {
+        ...state,
+        color: action.color
+      }
+    case FETCH_PHONES:
+      console.log('fetching phones');
+      return {
+        ...state,
+        items: action.payload
+      }
+    default:
+        return state;
   }
 }
 
